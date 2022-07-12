b70972f8f04ef4158a677ff85b72211f5b408b3d
tkong9 <taewon2711@gmail.com>
2022-07-12 23:38:47 +0900

    improve coordinate performance

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,29 +1,34 @@
 const followMouse = (event) => {
-  imageContainer.style.left = `${event.clientX}px`;
-  imageContainer.style.top = `${event.clientY}px`;
+  // imageContainer.style.left = `${event.clientX}px`;
+  // imageContainer.style.top = `${event.clientY}px`;
 
-  textContainer.style.left = `${event.clientX + 20}px`;
-  textContainer.style.top = `${event.clientY + 20}px`;
+  imageContainer.style.transform = `translate(${
+    event.clientX - imageHalfWidth
+  }px, ${event.clientY - imageHalfHeight}px)`;
 
-  vertical.style.left = `${event.clientX}px`;
-  vertical.style.top = `${event.clientY - window.innerHeight}px`;
+  textContainer.style.transform = `translate(${event.clientX + 20}px, ${
+    event.clientY + 20
+  }px)`;
 
-  horizontal.style.left = `${event.clientX - window.innerWidth}px`;
-  horizontal.style.top = `${event.clientY}px`;
+  vertical.style.transform = `translate(${event.clientX}px, ${
+    event.clientY - window.innerHeight
+  }px)`;
+
+  horizontal.style.transform = `translate(${
+    event.clientX - window.innerWidth
+  }px, ${event.clientY}px)`;
 
   textContainer.innerHTML = `${Math.round(event.clientX)}px, ${Math.round(
     event.clientY
   )}px`;
 };
 
-function mousemove(e) {
-  // console.log(`${e.clientX}, ${e.clientY}`);
-}
-
 let imageContainer = document.querySelector('#image');
 let textContainer = document.querySelector('#text');
 let vertical = document.querySelector('#vertical');
 let horizontal = document.querySelector('#horizontal');
 
-window.addEventListener('pointermove', mousemove);
+const imageHalfWidth = imageContainer.getBoundingClientRect().width / 2;
+const imageHalfHeight = imageContainer.getBoundingClientRect().height / 2;
+
 window.addEventListener('pointermove', followMouse);
