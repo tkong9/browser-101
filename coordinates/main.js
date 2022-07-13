1be2fb8055a85dc150e94d4809fb428fcdd54f28
tkong9 <taewon2711@gmail.com>
2022-07-13 11:39:17 +0900

    shopping list complete

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,7 +1,4 @@
 const followMouse = (event) => {
-  // imageContainer.style.left = `${event.clientX}px`;
-  // imageContainer.style.top = `${event.clientY}px`;
-
   imageContainer.style.transform = `translate(${
     event.clientX - imageHalfWidth
   }px, ${event.clientY - imageHalfHeight}px)`;
@@ -21,6 +18,21 @@ const followMouse = (event) => {
   textContainer.innerHTML = `${Math.round(event.clientX)}px, ${Math.round(
     event.clientY
   )}px`;
+};
+
+//layer shift가 일어나기 때문에 아래 처럼 코드를 작성하는것은 좋지 않다.
+const followMouse2 = (event) => {
+  imageContainer.style.left = `${event.clientX}px`;
+  imageContainer.style.top = `${event.clientY}px`;
+
+  textContainer.style.left = `${event.clientX + 20}px`;
+  textContainer.style.top = `${event.clientY + 20}px`;
+
+  vertical.style.left = `${event.clientX}px`;
+  vertical.style.top = `${event.clientY - window.innerHeight}px`;
+
+  horizontal.style.left = `${event.clientX - window.innerWidth}px`;
+  horizontal.style.top = `${event.clientY}px`;
 };
 
 let imageContainer = document.querySelector('#image');
---
+++ shopping-list/script.js
@@ -0,0 +1,24 @@
+const shoppingSection = document.querySelector('#shopping-list');
+const unorderedList = document.createElement('ul');
+shoppingSection.appendChild(unorderedList);
+
+const addBtn = document.querySelector('#add-btn');
+addBtn.addEventListener('click', addToList);
+
+const inputBox = document.querySelector('input');
+
+function addToList() {
+  if (inputBox.value == '') return;
+  const li = document.createElement('li');
+  li.textContent = inputBox.value;
+  const deleteBtn = document.createElement('button');
+  deleteBtn.type = 'button';
+  deleteBtn.textContent = 'X';
+  deleteBtn.setAttribute('class', 'delete-btn');
+  deleteBtn.addEventListener('click', () => {
+    li.remove();
+  });
+  li.appendChild(deleteBtn);
+  unorderedList.appendChild(li);
+  inputBox.value = '';
+}
