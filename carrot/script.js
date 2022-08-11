187788a63729566284bd37fb8b817a8358dd830d
tkong9 <taewon2711@gmail.com>
2022-08-12 00:45:04 +0900

    finish carrot game

--- carrot/script.js
+++ carrot/script.js
@@ -3,6 +3,12 @@ const playBtn = document.querySelector('#play-btn');
 const bugCatchCnt = document.querySelector('#bug-catch-counter');
 const catchArea = document.querySelector('#catch-area');
 
+const lostAudio = new Audio('./sound/alert.wav');
+const bgAudio = new Audio('./sound/bg.mp3');
+const bugAudio = new Audio('./sound/bug_pull.mp3');
+const carrotAudio = new Audio('./sound/carrot_pull.mp3');
+const winAudio = new Audio('./sound/game_win.mp3');
+
 // Timer
 let isPlaying = false;
 let remainingTime = 10;
@@ -14,31 +20,60 @@ gameTimer = setInterval(() => {
   if (isPlaying) {
     remainingTime -= 1;
     timer.innerHTML = `00:0${remainingTime}`;
-    if (remainingTime <= 0) {
-      isPlaying = false;
-      remainingTime = 10;
-      timer.innerHTML = '00:10';
-      playBtn.innerHTML = '►';
-      didGameEnd = true;
+    if (remainingTime == 0) {
+      endGameLost();
+    }
+    if (remainingTime >= 1 && remainingCarrotCnt == 0) {
+      endGameWin();
     }
   }
 }, 1000);
 
 playBtn.addEventListener('click', () => {
+  play();
+});
+
+function play() {
   isPlaying = !isPlaying;
   if (!isPlaying) {
+    bgAudio.pause();
     playBtn.innerHTML = '►';
   }
   if (isPlaying) {
+    bgAudio.play();
     playBtn.innerHTML = '◼︎';
     if (didGameEnd) {
       removeAllCarrontsAndBugs();
       generateCarrots();
       generateBugs();
       didGameEnd = !didGameEnd;
+      remainingCarrotCnt = 10;
+      remainingTime = 10;
+      timer.innerHTML = '00:10';
+      bugCatchCnt.innerHTML = `${remainingCarrotCnt}`;
     }
+    document.getElementById('lost').style.visibility = 'hidden';
+    document.getElementById('win').style.visibility = 'hidden';
   }
-});
+}
+
+function endGameLost() {
+  isPlaying = false;
+  playBtn.innerHTML = '►';
+  didGameEnd = true;
+  document.getElementById('lost').style.visibility = 'visible';
+  lostAudio.play();
+  bgAudio.pause();
+}
+
+function endGameWin() {
+  document.getElementById('win').style.visibility = 'visible';
+  isPlaying = false;
+  playBtn.innerHTML = '►';
+  didGameEnd = true;
+  winAudio.play();
+  bgAudio.pause();
+}
 
 // generate bugs and carrots
 bugCatchCnt.innerHTML = `${remainingCarrotCnt}`;
@@ -74,6 +109,10 @@ function makeCarrot(x, y) {
       carrot.remove();
       remainingCarrotCnt -= 1;
       bugCatchCnt.innerHTML = `${remainingCarrotCnt}`;
+      carrotAudio.play();
+      if (remainingCarrotCnt == 0 && remainingTime >= 1) {
+        endGameWin();
+      }
     }
   });
   return carrot;
@@ -86,6 +125,12 @@ function makeBug(x, y) {
   bug.className = 'bug-img';
   bug.style.left = `${x}px`;
   bug.style.top = `${y}px`;
+  bug.addEventListener('click', (e) => {
+    if (e.target == e.currentTarget) {
+      bugAudio.play();
+      endGameLost();
+    }
+  });
   return bug;
 }
 
@@ -93,13 +138,32 @@ function dispalyLost() {
   const lostWindow = document.createElement('div');
   lostWindow.id = 'lost';
 
-  const replaySpan = document.createElement('span');
+  const replaySpan = document.createElement('div');
   replaySpan.innerHTML = '►';
-  const lostSpan = document.createElement('span');
+  replaySpan.addEventListener('click', () => {
+    play();
+  });
+  const lostSpan = document.createElement('div');
   lostSpan.innerHTML = 'You lost';
   lostWindow.appendChild(replaySpan);
   lostWindow.appendChild(lostSpan);
   document.getElementById('background').appendChild(lostWindow);
+}
+
+function dispalyWin() {
+  const winWindow = document.createElement('div');
+  winWindow.id = 'win';
+
+  const replaySpan = document.createElement('div');
+  replaySpan.innerHTML = '►';
+  replaySpan.addEventListener('click', () => {
+    play();
+  });
+  const winSpan = document.createElement('div');
+  winSpan.innerHTML = 'You Won!!!';
+  winWindow.appendChild(replaySpan);
+  winWindow.appendChild(winSpan);
+  document.getElementById('background').appendChild(winWindow);
 }
 
 function removeAllCarrontsAndBugs() {
@@ -114,3 +178,6 @@ function removeAllCarrontsAndBugs() {
 }
 
 dispalyLost();
+dispalyWin();
+document.getElementById('lost').style.visibility = 'hidden';
+document.getElementById('win').style.visibility = 'hidden';
